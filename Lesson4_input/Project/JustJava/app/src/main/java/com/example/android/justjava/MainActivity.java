73ee2eb8f9fdfd96b9f23d940be64bb8821be566
Charles <dailiang18bb@gmail.com>
2018-02-12 18:12:34 -0500

    half way of the project

--- Lesson4_input/Project/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Lesson4_input/Project/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -28,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(-200);
+        int quantity = 5;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     /**
@@ -37,5 +41,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }

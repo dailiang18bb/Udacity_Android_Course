9ce37aa05fe49a5a0ec92c2f7b271a74f5413cfd
Charles <dailiang18bb@gmail.com>
2018-02-12 22:01:23 -0500

    update project
    +/- button

--- Lesson4_input/Project/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Lesson4_input/Project/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -50,4 +50,23 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
+
+
+    /**
+     * This method is called when the plus button clicked
+     */
+
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button clicked
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
+    }
+
 }

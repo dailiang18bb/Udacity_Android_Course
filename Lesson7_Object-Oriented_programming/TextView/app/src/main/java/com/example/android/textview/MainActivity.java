6381e2e12fd5822f86bb20d2b874be0ac22fdba8
Charles <dailiang18bb@gmail.com>
2018-03-05 21:08:10 -0500

    Lesson7_finished
    findViewById

--- Lesson7_Object-Oriented_programming/TextView/app/src/main/java/com/example/android/textview/MainActivity.java
+++ Lesson7_Object-Oriented_programming/TextView/app/src/main/java/com/example/android/textview/MainActivity.java
@@ -3,10 +3,21 @@ package com.example.android.textview;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    protected void onCreate(Bundle savedInstanceState) {
 
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+        View mView = findViewById(R.id.test);
+        mView.setVisibility(View.VISIBLE);
+    }
 }

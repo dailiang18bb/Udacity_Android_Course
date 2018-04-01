2aac75fee4ab61493e6873a6011b2321dcd3a5da
Charles <dailiang18bb@gmail.com>
2018-04-01 11:22:37 -0400

    seekBar change volume

--- Course4/MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ Course4/MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,22 +1,28 @@
 package com.example.android.musicplayer;
 
-import android.graphics.Bitmap;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
     private Button playBtn, pauseBtn;
     //
-    private String tilte;//歌名
-    private String artist;//歌手
-    private Bitmap bitmap;//专辑海报
-    private long duration;//时长
+    private AudioManager am;
+    private SeekBar seekBar;
+    //private VolumeReceiver receiver;
+    private ImageView mimageVolume;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +32,23 @@ public class MainActivity extends AppCompatActivity {
         playBtn = (Button) findViewById(R.id.playButton);
         pauseBtn = (Button) findViewById(R.id.pauseButton);
         mediaPlayer = MediaPlayer.create(this, R.raw.need_no_more);
+        seekBar = findViewById(R.id.changeVolume);
+        mimageVolume = findViewById(R.id.imageVolume);
 
 
+        //control the ImageView to show and disgard the seekbar
+        mimageVolume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (seekBar.getVisibility() == View.VISIBLE) {
+                    seekBar.setVisibility(View.GONE);
+                } else {
+                    seekBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        //handle the play btn
         playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //handle the pause btn
         pauseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,5 +66,55 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        /**
+         * change the system volume
+         */
+        am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        //get the System max volume
+        int maxVolume = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        seekBar.setMax(maxVolume);
+        //get the System current Volume
+        int currentVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        seekBar.setProgress(currentVolume);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    //set the system volume
+                    am.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                    int currentVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+                    seekBar.setProgress(currentVolume);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
     }
+
+
+
+//    private class VolumeReceiver extends BroadcastReceiver {
+//
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            if (intent.getAction().equals("android.media.VOLUME_CHANGED_ACTION")) {
+//                int currentVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+//                seekBar.setProgress(currentVolume);
+//
+//
+//            }
+//        }
+//    }
+
+
 }

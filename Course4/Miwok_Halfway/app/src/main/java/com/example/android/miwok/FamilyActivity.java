196e30aa034fadf033ec7603d4f3f9aa4852033b
Charles <dailiang18bb@gmail.com>
2018-04-19 21:24:30 -0400

    release at onStop()

--- Course4/Miwok_Halfway/app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ Course4/Miwok_Halfway/app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -55,6 +55,12 @@ public class FamilyActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
     /**
      * Clean up the media player by releasing its resources.
      */

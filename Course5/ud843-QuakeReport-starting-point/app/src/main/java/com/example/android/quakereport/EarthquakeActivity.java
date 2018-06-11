e3a21bcf25ad8e8733332ae571d5f7a148ae8527
Charles <dailiang18bb@gmail.com>
2018-06-10 23:14:41 -0400

    HTTP

--- Course5/ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ Course5/ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -38,14 +38,6 @@ public class EarthquakeActivity extends AppCompatActivity {
         // Create a fake list of earthquakes.
         final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
-//        earthquakes.add(new Earthquake("San Francisco", 7.2,"Feb 2, 2016"));
-//        earthquakes.add(new Earthquake("London", 6.1,"July 20, 2015"));
-//        earthquakes.add(new Earthquake("Tokyo", 3.9,"Nov 10, 2014"));
-//        earthquakes.add(new Earthquake("Mexico City", 5.4,"May 3, 2014"));
-//        earthquakes.add(new Earthquake("Moscow", 2.8,"Jan 31, 2013"));
-//        earthquakes.add(new Earthquake("Rio de Janeiro", 4.9,"Aug 19, 2012"));
-//        earthquakes.add(new Earthquake("Paris", 1.6,"Oct 30, 2011"));
-
         // Find a reference to the {@link ListView} in the layout
         final ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
